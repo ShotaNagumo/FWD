9ab8582005823f68d2c42b37b1244d7bac52e028
Shota NAGUMO <snagumo.maron+github@gmail.com>
2025-02-21 00:02:38 +0900

    doc: nagaoka_datamodel のpydoc記述

--- fwd/src/nagaoka_datamodel.py
+++ fwd/src/nagaoka_datamodel.py
@@ -20,48 +20,147 @@ class DatabaseInfo(Base):
         Base (_type_): declarative_base() によって取得した基底クラス
     """
 
+    """テーブル名
+    """
     __tablename__ = "_database_info"
+
+    """カラムID
+    """
     id = Column(Integer, primary_key=True)
+
+    """DBバージョン
+    """
     version = Column(Integer, nullable=False)
 
 
 class TextPosition(Enum):
+    """災害情報文字列が掲載されていた位置
+
+    Args:
+        Enum (_type_): Enum基底クラス
+    """
+
+    """「現在発生している災害」
+    """
     CURR = auto()
+
+    """「過去の災害経過情報」
+    """
     PAST = auto()
 
 
 class NotifyStatus(Enum):
+    """通知状態
+
+    Args:
+        Enum (_type_): Enum基底クラス
+    """
+
+    """通知不要
+    """
     SKIPPED = auto()
+
+    """通知未
+    """
     NOT_YET = auto()
+
+    """通知済み
+    """
     NOTIFIED = auto()
 
 
 class DisasterMainCategory(Enum):
+    """災害種別（大分類）
+
+    Args:
+        Enum (_type_): Enum基底クラス
+    """
+
+    """火災
+    """
     火災 = auto()
+
+    """救助
+    """
     救助 = auto()
+
+    """警戒
+    """
     警戒 = auto()
+
+    """救急支援
+    """
     救急支援 = auto()
+
+    """その他
+    """
     その他 = auto()
 
 
 class DisasterStatus(Enum):
+    """災害状態
+
+    Args:
+        Enum (_type_): Enum基底クラス
+    """
+
+    """災害発生
+    """
     発生 = auto()
+
+    """救助終了
+    """
     救助終了 = auto()
+
+    """消火不要
+    """
     消火不要 = auto()
+
+    """鎮圧
+    """
     鎮圧 = auto()
+
+    """鎮火
+    """
     鎮火 = auto()
+
+    """終了
+    """
     終了 = auto()
 
 
 class NagaokaRawText(Base):
+    """災害情報を格納するテーブル"""
+
+    """テーブル名
+    """
     __tablename__ = "nagaoka_raw_text"
+
+    """NagaokaRawText ID
+    """
     id = Column(Integer, primary_key=True, autoincrement=True)
+
+    """災害情報文字列
+    """
     raw_text = Column(String, nullable=False)
+
+    """取得時刻
+    （過去データから取得した場合は、過去データが記録されているファイル名から取得した日付）
+    """
     retr_dt = Column(DateTime, nullable=False, default=datetime.datetime.now())
+
+    """災害情報文字列が保存されていた位置
+    """
     text_pos = Column(sqlalchemy.Enum(TextPosition), nullable=False)
+
+    """災害情報の通知状態
+    """
     notify_status = Column(
         sqlalchemy.Enum(NotifyStatus), nullable=False, default=NotifyStatus.NOT_YET
     )
+
+    """詳細テーブルのオブジェクト
+    """
     detail_info = relationship("NagaokaDisasterDetail", uselist=False)
 
 
