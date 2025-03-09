266c1e6f8250629b082866f7679bf20b06354349
Shota NAGUMO <snagumo.maron+github@gmail.com>
2025-03-10 00:45:08 +0900

    fix: 鎮火情報の登録済み判定に使用する時刻情報の変数指定ミス修正

--- fwd/src/niigata_main.py
+++ fwd/src/niigata_main.py
@@ -270,7 +270,7 @@ class FwdNiigata:
                 # 登録済みかを確認する
                 # 12時間以内の鎮火情報を対象とする（鎮火情報には日付情報が無いため、
                 # 同一住所・同一時分の鎮火情報を混同する可能性があり、これを防ぐため）
-                threshold_dt = execute_dt - datetime.timedelta(hours=12)
+                threshold_dt = retrieve_dt - datetime.timedelta(hours=12)
                 registered = bool(
                     session.query(NiigataNoticeText)
                     .filter(NiigataNoticeText.raw_text == matches.group(0))
